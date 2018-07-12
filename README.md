a21becb5af361c4bb115375259a46b0a728b7d80
andy.rothwell <rothwell.andy@gmail.com>
2018-07-12 10:36:48 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# phila-vue-comps
+
+phila-vue-comps is a library of components that can be used in js apps
---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Badge from './components/Badge.vue';
+import BadgeCustom from './components/BadgeCustom.vue';
+import Callout from './components/Callout.vue';
+import CollectionSummary from './components/CollectionSummary.vue';
+import HorizontalTable from './components/HorizontalTable.vue';
+import HorizontalTableGroup from './components/HorizontalTableGroup.vue';
+import HorizontalTableRow from './components/HorizontalTableRow.vue';
+import Image from './components/Image.vue';
+import List from './components/List.vue';
+import OverlayToggleGroup from './components/OverlayToggleGroup.vue';
+import TabGroup from './components/TabGroup.vue';
+import VerticalTable from './components/VerticalTable.vue';
+
+
+exports.Badge = Badge;
+exports.BadgeCustom = BadgeCustom;
+exports.Callout = Callout;
+exports.CollectionSummary = CollectionSummary;
+exports.HorizontalTable = HorizontalTable;
+exports.HorizontalTableGroup = HorizontalTableGroup;
+exports.HorizontalTableRow = HorizontalTableRow;
+exports.Image = Image;
+exports.List = List;
+exports.OverlayToggleGroup = OverlayToggleGroup;
+exports.TabGroup = TabGroup;
+exports.VerticalTable = VerticalTable;
