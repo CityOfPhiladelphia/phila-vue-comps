0b706a3774679314253daa09430e245d6a13a679
andy.rothwell <rothwell.andy@gmail.com>
2018-12-03 11:04:25 -0500

    adds faTimesCircle icon

--- src/main.js
+++ src/main.js
@@ -11,9 +11,10 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faExternalLink } from '@fortawesome/pro-solid-svg-icons/faExternalLink';
 import { faSearch } from '@fortawesome/pro-solid-svg-icons/faSearch';
 import { faTimes } from '@fortawesome/pro-solid-svg-icons/faTimes';
+import { faTimesCircle } from '@fortawesome/pro-solid-svg-icons/faTimesCircle';
 import { faCaretLeft } from '@fortawesome/pro-solid-svg-icons/faCaretLeft';
 import { faCaretRight } from '@fortawesome/pro-solid-svg-icons/faCaretRight';
-library.add(faExternalLink, faSearch, faTimes, faCaretLeft, faCaretRight);
+library.add(faExternalLink, faSearch, faTimes, faTimesCircle, faCaretLeft, faCaretRight);
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 import AddressCandidateList from './components/AddressCandidateList.vue';
