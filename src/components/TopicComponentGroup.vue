d3aed56c7971ab7a8ffec8624cb59fac6f14bc6d
andy.rothwell <rothwell.andy@gmail.com>
2019-02-07 15:43:34 -0500

    uses webpack, conditionally imports most components into TopicComponentGroup

--- src/components/TopicComponentGroup.vue
+++ src/components/TopicComponentGroup.vue
@@ -14,51 +14,51 @@
 <script>
   import generateUniqueId from '../util/unique-id';
 
-  import AnyHeader from './AnyHeader.vue';
-  import Badge from './Badge.vue';
-  import ButtonComp from './ButtonComp.vue';
-  import Callout from './Callout.vue';
-  import CollectionSummary from './CollectionSummary.vue';
-  import Division from './Division.vue';
-  import ePayForm from './ePayForm.vue';
-  import ExternalLink from './ExternalLink.vue';
-  import HorizontalTable from './HorizontalTable.vue';
-  import Image_ from './Image.vue';
-  import VerticalTable from './VerticalTable.vue';
-  import OverlayToggleGroup from './OverlayToggleGroup.vue';
-  import Paragraph from './Paragraph.vue';
-  import SpanComp from './SpanComp.vue';
-  import List from './List.vue';
-  import TabGroup from './TabGroup.vue';
-  import HorizontalTableGroup from './HorizontalTableGroup.vue';
-  import BadgeCustom from './BadgeCustom.vue';
-  import Topic from './Topic.vue';
-  import TopicSet from './TopicSet.vue';
-  import PopoverLink from './PopoverLink.vue';
-  import Popover from './Popover.vue';
+  // import AnyHeader from './AnyHeader.vue';
+  // import Badge from './Badge.vue';
+  // import ButtonComp from './ButtonComp.vue';
+  // import Callout from './Callout.vue';
+  // import CollectionSummary from './CollectionSummary.vue';
+  // import Division from './Division.vue';
+  // import ePayForm from './ePayForm.vue';
+  // import ExternalLink from './ExternalLink.vue';
+  // import HorizontalTable from './HorizontalTable.vue';
+  // import Image_ from './Image.vue';
+  // import VerticalTable from './VerticalTable.vue';
+  // import OverlayToggleGroup from './OverlayToggleGroup.vue';
+  // import Paragraph from './Paragraph.vue';
+  // import SpanComp from './SpanComp.vue';
+  // import List from './List.vue';
+  // import TabGroup from './TabGroup.vue';
+  // import HorizontalTableGroup from './HorizontalTableGroup.vue';
+  // import BadgeCustom from './BadgeCustom.vue';
+  // import Topic from './Topic.vue';
+  // import TopicSet from './TopicSet.vue';
+  // import PopoverLink from './PopoverLink.vue';
+  // import Popover from './Popover.vue';
   // console.log('in TopicComponentGroup.vue script, BadgeCustom:', BadgeCustom, 'Badge:', Badge, 'TabGroup:', TabGroup, 'Callout:', Callout, 'CollectionSummary:', CollectionSummary, 'HorizontalTable:', HorizontalTable);
 
   export default {
     name: 'TopicComponentGroup',
     props: ['topicComponents', 'item', 'filterData'],
     components: {
-      Badge,
-      ButtonComp,
-      Callout,
-      CollectionSummary,
-      Division,
-      ePayForm,
-      ExternalLink,
-      HorizontalTable,
-      Image_,
-      VerticalTable,
-      OverlayToggleGroup,
-      Paragraph,
-      SpanComp,
-      List,
-      HorizontalTableGroup,
-      PopoverLink,
-      Popover,
+      Badge: () => import(/* webpackChunkName: "badge" */'./Badge.vue'),
+      ButtonComp: () => import(/* webpackChunkName: "buttonComp" */'./ButtonComp.vue'),
+      Callout: () => import(/* webpackChunkName: "callout" */'./Callout.vue'),
+      CollectionSummary: () => import(/* webpackChunkName: "collectionSummary" */'./CollectionSummary.vue'),
+      Division: () => import(/* webpackChunkName: "division" */'./Division.vue'),
+      ePayForm: () => import(/* webpackChunkName: "ePayForm" */'./ePayForm.vue'),
+      ExternalLink: () => import(/* webpackChunkName: "externalLink" */'./externalLink.vue'),
+      HorizontalTable: () => import(/* webpackChunkName: "horizontalTable" */'./HorizontalTable.vue'),
+      Image_: () => import(/* webpackChunkName: "image" */'./Image.vue'),
+      VerticalTable: () => import(/* webpackChunkName: "verticalTable" */'./VerticalTable.vue'),
+      OverlayToggleGroup: () => import(/* webpackChunkName: "overlayToggleGroup" */'./OverlayToggleGroup.vue'),
+      Paragraph: () => import(/* webpackChunkName: "paragraph" */'./Paragraph.vue'),
+      SpanComp: () => import(/* webpackChunkName: "spanComp" */'./SpanComp.vue'),
+      List: () => import(/* webpackChunkName: "list" */'./List.vue'),
+      HorizontalTableGroup: () => import(/* webpackChunkName: "horizontalTableGroup" */'./HorizontalTableGroup.vue'),
+      PopoverLink: () => import(/* webpackChunkName: "popoverLink" */'./PopoverLink.vue'),
+      Popover: () => import(/* webpackChunkName: "popover" */'./Popover.vue'),
     },
     beforeCreate() {
       // console.log('TopicComponentGroup beforeCreate is running');
