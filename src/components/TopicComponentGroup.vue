71c6d6cac4f2b4f06ba9cee0fb204161afe2ab15
andy.rothwell <rothwell.andy@gmail.com>
2019-02-08 08:29:27 -0500

    changes names of chunk files to trace them

--- src/components/TopicComponentGroup.vue
+++ src/components/TopicComponentGroup.vue
@@ -14,56 +14,32 @@
 <script>
   import generateUniqueId from '../util/unique-id';
 
-  // import AnyHeader from './AnyHeader.vue';
-  // import Badge from './Badge.vue';
-  // import ButtonComp from './ButtonComp.vue';
-  // import Callout from './Callout.vue';
-  // import CollectionSummary from './CollectionSummary.vue';
-  // import Division from './Division.vue';
-  // import ePayForm from './ePayForm.vue';
-  // import ExternalLink from './ExternalLink.vue';
-  // import HorizontalTable from './HorizontalTable.vue';
-  // import Image_ from './Image.vue';
-  // import VerticalTable from './VerticalTable.vue';
-  // import OverlayToggleGroup from './OverlayToggleGroup.vue';
-  // import Paragraph from './Paragraph.vue';
-  // import SpanComp from './SpanComp.vue';
-  // import List from './List.vue';
-  // import TabGroup from './TabGroup.vue';
-  // import HorizontalTableGroup from './HorizontalTableGroup.vue';
-  // import BadgeCustom from './BadgeCustom.vue';
-  // import Topic from './Topic.vue';
-  // import TopicSet from './TopicSet.vue';
-  // import PopoverLink from './PopoverLink.vue';
-  // import Popover from './Popover.vue';
-  // console.log('in TopicComponentGroup.vue script, BadgeCustom:', BadgeCustom, 'Badge:', Badge, 'TabGroup:', TabGroup, 'Callout:', Callout, 'CollectionSummary:', CollectionSummary, 'HorizontalTable:', HorizontalTable);
-
   export default {
     name: 'TopicComponentGroup',
     props: ['topicComponents', 'item', 'filterData'],
     components: {
-      Badge: () => import(/* webpackChunkName: "badge" */'./Badge.vue'),
-      ButtonComp: () => import(/* webpackChunkName: "buttonComp" */'./ButtonComp.vue'),
-      Callout: () => import(/* webpackChunkName: "callout" */'./Callout.vue'),
-      CollectionSummary: () => import(/* webpackChunkName: "collectionSummary" */'./CollectionSummary.vue'),
-      Division: () => import(/* webpackChunkName: "division" */'./Division.vue'),
-      ePayForm: () => import(/* webpackChunkName: "ePayForm" */'./ePayForm.vue'),
-      ExternalLink: () => import(/* webpackChunkName: "externalLink" */'./externalLink.vue'),
-      HorizontalTable: () => import(/* webpackChunkName: "horizontalTable" */'./HorizontalTable.vue'),
-      Image_: () => import(/* webpackChunkName: "image" */'./Image.vue'),
-      VerticalTable: () => import(/* webpackChunkName: "verticalTable" */'./VerticalTable.vue'),
-      OverlayToggleGroup: () => import(/* webpackChunkName: "overlayToggleGroup" */'./OverlayToggleGroup.vue'),
-      Paragraph: () => import(/* webpackChunkName: "paragraph" */'./Paragraph.vue'),
-      SpanComp: () => import(/* webpackChunkName: "spanComp" */'./SpanComp.vue'),
-      List: () => import(/* webpackChunkName: "list" */'./List.vue'),
-      HorizontalTableGroup: () => import(/* webpackChunkName: "horizontalTableGroup" */'./HorizontalTableGroup.vue'),
-      PopoverLink: () => import(/* webpackChunkName: "popoverLink" */'./PopoverLink.vue'),
-      Popover: () => import(/* webpackChunkName: "popover" */'./Popover.vue'),
-      TabGroup: () => import(/* webpackChunkName: "tabGroup" */'./TabGroup.vue'),
-      BadgeCustom: () => import(/* webpackChunkName: "badgeCustom" */'./BadgeCustom.vue'),
-      Topic: () => import(/* webpackChunkName: "topic" */'./Topic.vue'),
-      TopicSet: () => import(/* webpackChunkName: "topicSet" */'./TopicSet.vue'),
-      AnyHeader: () => import(/* webpackChunkName: "anyHeader" */'./AnyHeader.vue'),
+      Badge: () => import(/* webpackChunkName: "tcg_pvc_Badge" */'./Badge.vue'),
+      ButtonComp: () => import(/* webpackChunkName: "tcg_pvc_ButtonComp" */'./ButtonComp.vue'),
+      Callout: () => import(/* webpackChunkName: "tcg_pvc_Callout" */'./Callout.vue'),
+      CollectionSummary: () => import(/* webpackChunkName: "tcg_pvc_CollectionSummary" */'./CollectionSummary.vue'),
+      Division: () => import(/* webpackChunkName: "tcg_pvc_Division" */'./Division.vue'),
+      ePayForm: () => import(/* webpackChunkName: "tcg_pvc_ePayForm" */'./ePayForm.vue'),
+      ExternalLink: () => import(/* webpackChunkName: "tcg_pvc_ExternalLink" */'./externalLink.vue'),
+      HorizontalTable: () => import(/* webpackChunkName: "tcg_pvc_HorizontalTable" */'./HorizontalTable.vue'),
+      Image_: () => import(/* webpackChunkName: "tcg_pvc_Image" */'./Image.vue'),
+      VerticalTable: () => import(/* webpackChunkName: "tcg_pvc_VerticalTable" */'./VerticalTable.vue'),
+      OverlayToggleGroup: () => import(/* webpackChunkName: "tcg_pvc_OverlayToggleGroup" */'./OverlayToggleGroup.vue'),
+      Paragraph: () => import(/* webpackChunkName: "tcg_pvc_Paragraph" */'./Paragraph.vue'),
+      SpanComp: () => import(/* webpackChunkName: "tcg_pvc_SpanComp" */'./SpanComp.vue'),
+      List: () => import(/* webpackChunkName: "tcg_pvc_List" */'./List.vue'),
+      HorizontalTableGroup: () => import(/* webpackChunkName: "tcg_pvc_HorizontalTableGroup" */'./HorizontalTableGroup.vue'),
+      PopoverLink: () => import(/* webpackChunkName: "tcg_pvc_PopoverLink" */'./PopoverLink.vue'),
+      Popover: () => import(/* webpackChunkName: "tcg_pvc_Popover" */'./Popover.vue'),
+      TabGroup: () => import(/* webpackChunkName: "tcg_pvc_TabGroup" */'./TabGroup.vue'),
+      BadgeCustom: () => import(/* webpackChunkName: "tcg_pvc_BadgeCustom" */'./BadgeCustom.vue'),
+      Topic: () => import(/* webpackChunkName: "tcg_pvc_Topic" */'./Topic.vue'),
+      TopicSet: () => import(/* webpackChunkName: "tcg_pvc_TopicSet" */'./TopicSet.vue'),
+      AnyHeader: () => import(/* webpackChunkName: "tcg_pvc_AnyHeader" */'./AnyHeader.vue'),
     },
     beforeCreate() {
       // console.log('TopicComponentGroup beforeCreate is running');
