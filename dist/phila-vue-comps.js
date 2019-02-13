69630768c69580cd1e5764f4b7b1e94b60f19dfb
andy.rothwell <rothwell.andy@gmail.com>
2019-02-13 16:10:02 -0500

    new build

--- dist/phila-vue-comps.js
+++ dist/phila-vue-comps.js
@@ -1,8 +1,8 @@
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('lodash.debounce'), require('axios'), require('date-fns'), require('escape-html'), require('jspdf'), require('jspdf-autotable'), require('@fortawesome/fontawesome-svg-core'), require('@fortawesome/vue-fontawesome')) :
-	typeof define === 'function' && define.amd ? define(['exports', 'lodash.debounce', 'axios', 'date-fns', 'escape-html', 'jspdf', 'jspdf-autotable', '@fortawesome/fontawesome-svg-core', '@fortawesome/vue-fontawesome'], factory) :
-	(factory((global.philaVueComps = {}),global.debounce,global.axios,global.dateFns,global.escapeHtml,global.jsPDF,global.jspdfAutotable,global.fontawesomeSvgCore,global.vueFontAwesome));
-}(this, (function (exports,debounce,axios,dateFns,escapeHtml,jsPDF,jspdfAutotable,fontawesomeSvgCore,vueFontawesome) { 'use strict';
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('lodash.debounce'), require('axios'), require('date-fns'), require('escape-html'), require('jspdf'), require('jspdf-autotable'), require('@fortawesome/fontawesome-svg-core'), require('@fortawesome/free-solid-svg-icons/faExternalLink'), require('@fortawesome/vue-fontawesome')) :
+	typeof define === 'function' && define.amd ? define(['exports', 'lodash.debounce', 'axios', 'date-fns', 'escape-html', 'jspdf', 'jspdf-autotable', '@fortawesome/fontawesome-svg-core', '@fortawesome/free-solid-svg-icons/faExternalLink', '@fortawesome/vue-fontawesome'], factory) :
+	(factory((global.philaVueComps = {}),global.debounce,global.axios,global.dateFns,global.escapeHtml,global.jsPDF,global.jspdfAutotable,global.fontawesomeSvgCore,global.faExternalLink,global.vueFontAwesome));
+}(this, (function (exports,debounce,axios,dateFns,escapeHtml,jsPDF,jspdfAutotable,fontawesomeSvgCore,faExternalLink,vueFontawesome) { 'use strict';
 
 	debounce = debounce && debounce.hasOwnProperty('default') ? debounce['default'] : debounce;
 	axios = axios && axios.hasOwnProperty('default') ? axios['default'] : axios;
@@ -17,48 +17,6 @@
 	function createCommonjsModule(fn, module) {
 		return module = { exports: {} }, fn(module, module.exports), module.exports;
 	}
-
-	var faExternalLink = createCommonjsModule(function (module, exports) {
-	Object.defineProperty(exports, '__esModule', { value: true });
-	var prefix = 'fas';
-	var iconName = 'external-link';
-	var width = 576;
-	var height = 512;
-	var ligatures = [];
-	var unicode = 'f08e';
-	var svgPathData = 'M448 279.196V464c0 26.51-21.49 48-48 48H48c-26.51 0-48-21.49-48-48V112c0-26.51 21.49-48 48-48h248a24 24 0 0 1 16.97 7.029l16 16C344.09 102.149 333.382 128 312 128H64v320h320V295.196c0-6.365 2.529-12.47 7.029-16.971l16-16C422.148 247.106 448 257.814 448 279.196zM576 37.333C576 16.715 559.285 0 538.667 0H380c-15.464 0-28 12.536-28 28v17.885c0 15.766 13.011 28.424 28.772 27.989l67.203-1.906L199.09 319.09c-9.429 9.363-9.457 24.605-.061 34.001l23.879 23.879c9.396 9.396 24.639 9.369 34.001-.06l247.122-248.885-1.906 67.203c-.434 15.76 12.224 28.772 27.99 28.772H548c15.464 0 28-12.536 28-28V37.333z';
-
-	exports.definition = {
-	  prefix: prefix,
-	  iconName: iconName,
-	  icon: [
-	    width,
-	    height,
-	    ligatures,
-	    unicode,
-	    svgPathData
-	  ]};
-
-	exports.faExternalLink = exports.definition;
-	exports.prefix = prefix;
-	exports.iconName = iconName;
-	exports.width = width;
-	exports.height = height;
-	exports.ligatures = ligatures;
-	exports.unicode = unicode;
-	exports.svgPathData = svgPathData;
-	});
-
-	unwrapExports(faExternalLink);
-	var faExternalLink_1 = faExternalLink.definition;
-	var faExternalLink_2 = faExternalLink.faExternalLink;
-	var faExternalLink_3 = faExternalLink.prefix;
-	var faExternalLink_4 = faExternalLink.iconName;
-	var faExternalLink_5 = faExternalLink.width;
-	var faExternalLink_6 = faExternalLink.height;
-	var faExternalLink_7 = faExternalLink.ligatures;
-	var faExternalLink_8 = faExternalLink.unicode;
-	var faExternalLink_9 = faExternalLink.svgPathData;
 
 	var faSearch = createCommonjsModule(function (module, exports) {
 	Object.defineProperty(exports, '__esModule', { value: true });
@@ -3620,7 +3578,7 @@
 	|   __/|___|  /__|____(____  /           \_/ |____/  \___  >          \___  >____/|__|_|  /   __/____  >
 	|__|        \/             \/                            \/               \/            \/|__|       \/
 	*/
-	fontawesomeSvgCore.library.add(faExternalLink_2, faSearch_2, faTimes_2, faTimesCircle_2, faCaretLeft_2, faCaretRight_2);
+	fontawesomeSvgCore.library.add(faExternalLink.faExternalLink, faSearch_2, faTimes_2, faTimesCircle_2, faCaretLeft_2, faCaretRight_2);
 
 	exports.pvcStore = pvmStore;
 	exports.AddressCandidateList = AddressCandidateList;
