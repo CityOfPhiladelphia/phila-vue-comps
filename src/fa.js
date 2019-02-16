0f4dd02e5c2ac984c87e88b924438fbaf16c04a4
andy\Andy Rothwell <rothwell.andy@gmail.com>
2019-02-16 17:01:14 -0500

    fixes font awesome stuff

--- src/fa.js
+++ src/fa.js
@@ -1,12 +1,12 @@
 // Font Awesome Icons
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faExternalLink } from '@fortawesome/pro-solid-svg-icons/faExternalLink';
+import { faExternalLinkAlt } from '@fortawesome/pro-solid-svg-icons/faExternalLinkAlt';
 import { faSearch } from '@fortawesome/pro-solid-svg-icons/faSearch';
 import { faTimes } from '@fortawesome/pro-solid-svg-icons/faTimes';
 import { faTimesCircle } from '@fortawesome/pro-solid-svg-icons/faTimesCircle';
 import { faCaretLeft } from '@fortawesome/pro-solid-svg-icons/faCaretLeft';
 import { faCaretRight } from '@fortawesome/pro-solid-svg-icons/faCaretRight';
-library.add(faExternalLink, faSearch, faTimes, faTimesCircle, faCaretLeft, faCaretRight);
+library.add(faExternalLinkAlt, faSearch, faTimes, faTimesCircle, faCaretLeft, faCaretRight);
 
 export default library;
