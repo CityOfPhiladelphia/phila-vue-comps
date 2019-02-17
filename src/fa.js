dbb1bbe780a7d34a7a6fe1b131cd00fa38bbd52c
andy\Andy Rothwell <rothwell.andy@gmail.com>
2019-02-17 17:54:00 -0500

    only uses free font awesome icons

--- src/fa.js
+++ src/fa.js
@@ -1,12 +1,12 @@
 // Font Awesome Icons
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faExternalLinkAlt } from '@fortawesome/pro-solid-svg-icons/faExternalLinkAlt';
-import { faSearch } from '@fortawesome/pro-solid-svg-icons/faSearch';
-import { faTimes } from '@fortawesome/pro-solid-svg-icons/faTimes';
-import { faTimesCircle } from '@fortawesome/pro-solid-svg-icons/faTimesCircle';
-import { faCaretLeft } from '@fortawesome/pro-solid-svg-icons/faCaretLeft';
-import { faCaretRight } from '@fortawesome/pro-solid-svg-icons/faCaretRight';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons/faExternalLinkAlt';
+import { faSearch } from '@fortawesome/free-solid-svg-icons/faSearch';
+import { faTimes } from '@fortawesome/free-solid-svg-icons/faTimes';
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons/faTimesCircle';
+import { faCaretLeft } from '@fortawesome/free-solid-svg-icons/faCaretLeft';
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons/faCaretRight';
 library.add(faExternalLinkAlt, faSearch, faTimes, faTimesCircle, faCaretLeft, faCaretRight);
 
 export default library;
