ab30addaf362f6e8b3572dd1aebfc7e10822a649
andy.rothwell <rothwell.andy@gmail.com>
2019-03-14 13:25:46 -0400

    fixes FullScreenTopicsToggleTab

--- src/components/FullScreenTopicsToggleTab.vue
+++ src/components/FullScreenTopicsToggleTab.vue
@@ -14,28 +14,31 @@
 <script>
   export default {
     name: 'FullScreenTopicsToggleTab',
-    props: {
-      elementContainer: {
-        type: String,
-        default: 'topic-panel-container'
-      }
-    },
     data() {
       return {
-        'divHeight': 0,
         'buttonPosition': 0,
       }
     },
     mounted() {
-      window.addEventListener('resize', this.setDivHeight);
-      this.setDivHeight();
+      this.setDivHeight(this.windowDim);
+    },
+    watch: {
+      picOrCycloActive() {
+        this.setDivHeight(this.windowDim);
+      },
+      windowDim(nextDim) {
+        this.setDivHeight(nextDim);
+      }
     },
     computed: {
+      windowDim() {
+        return this.$store.state.windowDimensions;
+      },
       fullScreenMapEnabled() {
         return this.$store.state.fullScreenMapEnabled;
       },
       fullScreenTopicsEnabled() {
-        console.log('this.$store.state.fullScreenTopicsEnabled:', this.$store.state.fullScreenTopicsEnabled);
+        // console.log('this.$store.state.fullScreenTopicsEnabled:', this.$store.state.fullScreenTopicsEnabled);
         return this.$store.state.fullScreenTopicsEnabled;// || true;
       },
       isMobileOrTablet() {
@@ -70,30 +73,13 @@
         }
       }
     },
-    watch: {
-      picOrCycloActive() {
-        this.$nextTick(() => {
-          this.setDivHeight();
-        })
-      }
-    },
     methods: {
-      setDivHeight() {
-        let el;
-        if (this.fullScreenTopicsEnabled) {
-          el = document.getElementById(this.$props.elementContainer);
+      setDivHeight(dim) {
+        if (!this.picOrCycloActive) {
+          this.buttonPosition = (dim.height-48)/2 + 'px';
         } else {
-          el = document.getElementById('map-tag');
-          if (el === null) {
-            el = document.getElementById('topic-panel-container');
-          }
+          this.buttonPosition = (dim.height-48)/4 + 'px';
         }
-        // console.log('in FullScreenTopicsToggleTab, setDivHeight, el:', el)
-        const mapDivStyle = window.getComputedStyle(el);
-        const mapDivHeight = parseFloat(mapDivStyle.getPropertyValue('height').replace('px', ''));
-        // console.log('FullScreenTopicsToggleTab setDivHeight is running, el:', el, 'mapDivHeight:', mapDivHeight);
-
-        this.buttonPosition = (mapDivHeight-48)/2 + 'px';
       },
       handleFullScreenTopicsToggleButtonClick(e) {
         const prevFullScreenTopicsEnabled = this.$store.state.fullScreenTopicsEnabled;
@@ -105,21 +91,6 @@
 </script>
 
 <style scoped>
-
-  /* .toggle-tab {
-    position: absolute;
-    left: 0px;
-    border-width: 1.3px;
-    border-color: #CAC9C9;
-    height: 48px;
-    line-height: 58px;
-    width:24px;
-    background-color: white;
-    display: inline-block;
-    z-index: 500; */
-    /* border-left-style: solid; */
-    /* box-shadow: 2px 2px 7px grey; */
-  /* } */
 
   .toggle-tab {
     display: none;
@@ -143,8 +114,6 @@
       display: inline-block;
       z-index: 500;
       opacity: 0.7;
-      /* border-left-style: solid; */
-      /* box-shadow: 2px 2px 7px grey; */
     }
   }
 
