7eb69e071151811858f8392f39df66b2d7b5e0df
andy.rothwell <rothwell.andy@gmail.com>
2019-08-08 09:55:56 -0400

    changes to fix external link icons

--- src/fa.js
+++ src/fa.js
@@ -1,13 +1,15 @@
 // Font Awesome Icons
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faExternalLink } from '@fortawesome/free-solid-svg-icons/faExternalLink';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons/faExternalLinkAlt';
 import { faSearch } from '@fortawesome/free-solid-svg-icons/faSearch';
 import { faTimes } from '@fortawesome/free-solid-svg-icons/faTimes';
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons/faTimesCircle';
 import { faCaretLeft } from '@fortawesome/free-solid-svg-icons/faCaretLeft';
 import { faCaretRight } from '@fortawesome/free-solid-svg-icons/faCaretRight';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons/faCaretDown';
+import { faCaretUp } from '@fortawesome/free-solid-svg-icons/faCaretUp';
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons/faInfoCircle';
-library.add(faExternalLink, faSearch, faTimes, faTimesCircle, faCaretLeft, faCaretRight, faInfoCircle);
+library.add(faExternalLinkAlt, faSearch, faTimes, faTimesCircle, faCaretLeft, faCaretRight, faCaretDown, faCaretUp, faInfoCircle);
 
 export default library;
