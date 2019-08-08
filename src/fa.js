98c1295aa9d737293333d4d047f4d52ba1609a61
andy.rothwell <rothwell.andy@gmail.com>
2019-08-08 09:26:28 -0400

    imports font awesome icons from @fortawesome/free-solid-svg-icons

--- src/fa.js
+++ src/fa.js
@@ -1,13 +1,13 @@
 // Font Awesome Icons
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faExternalLink } from '@fortawesome/pro-solid-svg-icons/faExternalLink';
-import { faSearch } from '@fortawesome/pro-solid-svg-icons/faSearch';
-import { faTimes } from '@fortawesome/pro-solid-svg-icons/faTimes';
-import { faTimesCircle } from '@fortawesome/pro-solid-svg-icons/faTimesCircle';
-import { faCaretLeft } from '@fortawesome/pro-solid-svg-icons/faCaretLeft';
-import { faCaretRight } from '@fortawesome/pro-solid-svg-icons/faCaretRight';
-import { faInfoCircle } from '@fortawesome/pro-solid-svg-icons/faInfoCircle';
+import { faExternalLink } from '@fortawesome/free-solid-svg-icons/faExternalLink';
+import { faSearch } from '@fortawesome/free-solid-svg-icons/faSearch';
+import { faTimes } from '@fortawesome/free-solid-svg-icons/faTimes';
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons/faTimesCircle';
+import { faCaretLeft } from '@fortawesome/free-solid-svg-icons/faCaretLeft';
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons/faCaretRight';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons/faInfoCircle';
 library.add(faExternalLink, faSearch, faTimes, faTimesCircle, faCaretLeft, faCaretRight, faInfoCircle);
 
 export default library;
