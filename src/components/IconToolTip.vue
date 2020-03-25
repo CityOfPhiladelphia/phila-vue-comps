562158f392f99f5d397462ba1cfc81ed7ffaee1b
Andy Rothwell <rothwell.andy@gmail.com>
2020-03-25 14:28:34 -0400

    fixes style for icontooltip

--- src/components/IconToolTip.vue
+++ src/components/IconToolTip.vue
@@ -6,10 +6,10 @@
       :id="item + '-icon'"
       icon="info-circle"
       class="fa-infoCircle"
-      @click="infoClick"
+      @mouseover="infoOn"
+      @mouseout="infoOff"
     />
-    <!-- @mouseover="infoOn"
-    @mouseout="infoOff" -->
+    <!-- @click="infoClick" -->
     <div
       :id="item + '-tooltip'"
       :class="tipClass"
@@ -37,32 +37,9 @@ export default {
     return {
       active: false,
       tipClass: 'popup-tip',
-      tooltipStyle: {
-        // 'right': '10px',
-      },
+      tooltipStyle: {},
     }
   },
-  // mounted() {
-  //   const thisId = this.$props.item + '-icon';
-  //   const thisIcon = document.getElementById(thisId);
-  //   const iconStyle = thisIcon.getBoundingClientRect();
-  //   const rootElement = document.getElementById('app');
-  //   const rootStyle = window.getComputedStyle(rootElement);
-  //   const rootWidth = rootStyle.getPropertyValue('width');
-  //   const rootWidthNum = parseInt(rootWidth.replace('px', ''));
-  //   const gap = rootWidthNum - iconStyle.right;
-  //   console.log('iconToolTip mounted, iconStyle:', iconStyle, 'rootWidthNum:', rootWidthNum, 'gap:', gap);
-  //   if (gap < 250) {
-  //     this.tooltipStyle = {
-  //       'top': iconStyle.top + 30 + 'px',
-  //       'right': '10px',
-  //     }
-  //   } else {
-  //     this.tooltipStyle = {
-  //       'top': iconStyle.top + 20 + 'px',
-  //     }
-  //   }
-  // },
   methods: {
     infoClick(e) {
       this.active = !this.active;
@@ -85,16 +62,6 @@ export default {
           'top': iconStyle.top + 20 + 'px',
         }
       }
-      // const thisId = this.$props.item + '-tooltip';
-      // const thisToolTip = document.getElementById(thisId);
-      // const rootElement = document.getElementById('app');
-      // const rootStyle = window.getComputedStyle(rootElement);
-      // const rootWidth = rootStyle.getPropertyValue('width');
-      // // const rootHeight = rootStyle.getPropertyValue('height');
-      // const rootWidthNum = parseInt(rootWidth.replace('px', ''));
-      // // const rootHeightNum = parseInt(rootHeight.replace('px', ''));
-      // console.log('infoClick is running, e:', e, 'e.x', e.x, 'thisToolTip:', thisToolTip, 'rootWidthNum:', rootWidthNum);
-      // this.tipClass = 'popup-tip right-side'
     },
     infoOn() {
       this.active = true;
@@ -147,23 +114,33 @@ export default {
 }
 
 .full-div {
-  padding: 5px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  padding-left: 10px;
+  padding-right: 10px;
 }
 
 .half-div {
   position: relative;
   display: inline-block;
-  padding: 5px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  padding-left: 10px;
+  padding-right: 10px;
   width: 110px;
   background-color: rgb(15, 77, 144);
   color: white;
-  /* z-index: 10002; */
+}
+
+ul {
+  margin-bottom: 0px;
 }
 
 @media screen and (max-width: 749px) {
   .popup-tip {
     position: absolute;
     top: 10px !important;
+    right: 10px !important;
     display: inline-block;
     margin-left: 5px;
     width: 220px;
